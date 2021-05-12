9199e725802ecea7af73d0610f8422b0b1e0a3a9
Alyson Ronnan Martins <alysonronnan@gmail.com>
2021-05-11 21:27:34 -0300

    Aparencia de desenvolviemento parcialmente pronta

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import styled from 'styled-components'
 
-const Title = styled.h1`
-	font-size: 64px;
-	color: #8752e6;
-`
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-    	<Title>Hello Word</Title>
-    </div>
+    <>
+    	<Header/>
+      <Dashboard/>
+    	<GlobalStyle/>
+    </>
   );
 }
 
